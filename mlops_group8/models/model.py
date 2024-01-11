a2dfd5fa288767228c9b370c520f7efaf98d4f95
Lucasmogsan <lucassandby@gmail.com>
2024-01-11 16:22:07 +0100

    combining Hydra, pre, wand

--- mlops_group8/models/model.py
+++ mlops_group8/models/model.py
@@ -1,13 +1,15 @@
 import torch
 
+
 class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class.
+    """Basic neural network class.
 
     Args:
         in_features: number of input features
         out_features: number of output features
 
     """
+
     def __init__(self, in_features: int, out_features: int) -> None:
         self.l1 = torch.nn.Linear(in_features, 500)
         self.l2 = torch.nn.Linear(500, out_features)
